9d856f9c8462c77c85c364f7717308a8a500a4e2
Alexis Diaz <alexdan1993@gmail.com>
2023-09-29 15:39:12 -0300

    Testing prettier

---
+++ src/components/some-comp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SomeCompWhatever = () => {
+  return (
+    <div>
+      <p>Im a component yo look at me</p>
+    </div>
+  );
+};
+
+export default SomeCompWhatever;
