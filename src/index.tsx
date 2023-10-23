933b0a5648a2100495451397eaf8f32b7a237754
Alexis Diaz <alexdan1993@gmail.com>
2023-10-23 20:28:13 -0300

    Adding sentry

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import './index.css';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import { fetchTasks } from './features/task-slice';
+import * as Sentry from '@sentry/react';
 
 const environment = process.env.NODE_ENV;
 makeServer({ environment });
@@ -19,6 +20,22 @@ const root = ReactDOM.createRoot(
 );
 
 store.dispatch(fetchTasks());
+
+Sentry.init({
+  dsn: 'https://57e08721f5b1a9f245e19b053c0282c2@o4506102178578432.ingest.sentry.io/4506102180413440',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 root.render(
   <React.StrictMode>
